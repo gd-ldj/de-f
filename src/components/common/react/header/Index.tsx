426834739a6a4683c11021ab413b82e223baa1d0
Derek Li <derekli2024@gmail.com>
2025-09-01 11:51:58 +0800

    feat: update logo

--- src/components/common/react/header/Index.tsx
+++ src/components/common/react/header/Index.tsx
@@ -265,7 +265,7 @@ export default function Header({ userComponent }: HeaderProps) {
           {/* Logo - Center */}
           <div className="flex items-center justify-center flex-shrink-0">
             <a href={`/${locale}`} className="flex items-center">
-              <img src={DetakeLogo} alt="logo" className="w-38" />
+              <img src={DetakeLogo} alt="logo" className="w-32" />
             </a>
           </div>
 
